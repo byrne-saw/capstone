0f9a8cdb2a8eb47690e9e68a1022a042fb1c0d25
byrne-saw <byrnes.aw@gmail.com>
2018-10-07 13:55:27 -0500

    adding password_digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_183045) do
+ActiveRecord::Schema.define(version: 2018_10_07_185237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_10_06_183045) do
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
