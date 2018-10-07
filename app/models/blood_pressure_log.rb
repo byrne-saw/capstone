819b9f2e709d821b27979218583b4b38b06fad91
byrne-saw <byrnes.aw@gmail.com>
2018-10-07 15:14:09 -0500

    seeding BloodPressureLog

--- app/models/blood_pressure_log.rb
+++ app/models/blood_pressure_log.rb
@@ -2,8 +2,8 @@ class BloodPressureLog < ApplicationRecord
   validates :user_id, presence: true
   validates :log_time, presence: true
   validates :systolic, presence: true
-  validates :systolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 2 }
+  validates :systolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 200 }
   validates :diastolic, presence: true
-  validates :diastolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 2 }
+  validates :diastolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 200 }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# log = BloodPressureLog.new(
+#                           user_id: 3,
+#                           log_time: "2018-10-07 15:04:12 -0500",
+#                           systolic: 120,
+#                           diastolic: 80
+#                           )
+# log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-06 15:04:12 -0500",
+                          systolic: 125,
+                          diastolic: 85
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-05 14:04:12 -0500",
+                          systolic: 130,
+                          diastolic: 82
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-04 12:04:12 -0500",
+                          systolic: 142,
+                          diastolic: 90
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-03 08:04:12 -0500",
+                          systolic: 130,
+                          diastolic: 87
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-02 18:04:12 -0500",
+                          systolic: 132,
+                          diastolic: 91
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-10-01 12:59:12 -0500",
+                          systolic: 119,
+                          diastolic: 79
+                          )
+log.save
+
+log = BloodPressureLog.new(
+                          user_id: 3,
+                          log_time: "2018-09-30 13:10:12 -0500",
+                          systolic: 121,
+                          diastolic: 81
+                          )
+log.save
