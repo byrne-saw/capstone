a2d25234a9104face54e426a368d738261a5eba9
byrne-saw <byrnes.aw@gmail.com>
2018-10-07 14:03:51 -0500

    adding validations

--- app/models/blood_pressure_log.rb
+++ app/models/blood_pressure_log.rb
@@ -1,2 +1,9 @@
 class BloodPressureLog < ApplicationRecord
+  validates :user_id, presence: true
+  validates :log_time, presence: true
+  validates :systolic, presence: true
+  validates :systolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 2 }
+  validates :diastolic, presence: true
+  validates :diastolic, numericality: { greater_than: 0, only_integer: true, greater_than_or_equal_to: 1, less_than: 2 }
+
 end
