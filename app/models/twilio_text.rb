fb5393c0cc06515148c24d1a1ef2ace078bfbd9a
byrne-saw <byrnes.aw@gmail.com>
2018-11-13 17:54:33 -0600

    last minute deploy

--- app/models/twilio_text.rb
+++ app/models/twilio_text.rb
@@ -1,11 +1,7 @@
 class TwilioText 
-  attr_reader :message
-  attr_reader :phone_number
 
   def initialize(message, phone_number)
     @message = message
-    # @phone_number = phone_number.delete("^0-9")[0..9]
-    # @phone_number = "+1" + phone_number.delete("^0-9")[0..9]
     @phone_number = phone_number
   end
 
@@ -13,9 +9,8 @@ class TwilioText
     client = Twilio::REST::Client.new
     client.messages.create({
       from: Rails.application.credentials.twilio_phone_number,
-      # to: '+17738853893', #for testing
-      to: phone_number,
-      body: message
+      to: @phone_number,
+      body: @message
     })
   end
 end
